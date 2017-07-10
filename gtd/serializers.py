cf0fb15a56678274242f48d2f723b80639a8d6c4
Jimbo Freedman <github@jlf.me>
2017-07-10 23:25:55 +0100

    Return all Action fields in API

--- gtd/serializers.py
+++ gtd/serializers.py
@@ -4,4 +4,4 @@ from .models import Action
 class ActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Action
-        fields = ['id', 'short_description', 'status', 'priority']
+        fields = ['id', 'short_description', 'status', 'priority', 'start_at', 'due_at', 'recurrence']
