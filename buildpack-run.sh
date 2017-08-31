1b5673f5bd45d57ea0c1811893b21c6e2ce5d1d3
Jimbo Freedman <github@jlf.me>
2017-08-31 20:44:32 +0100

    Install graphviz on deploy

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -3,19 +3,19 @@ install_dir=graphviz
 wget http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.40.1.tar.gz
 tar -zxf graphviz-2.40.1.tar.gz
 cd graphviz-2.40.1
-./configure --prefix=/app/$install_dir
+./configure --prefix=$BUILD_DIR/$install_dir
 make
 make install
 
 mkdir -p $BUILD_DIR/.profile.d
 echo $BUILD_DIR
 script=$BUILD_DIR/.profile.d/graphviz.sh
-echo "PATH=/app/$install_dir/usr/bin:\$PATH" >"$script"
+echo "PATH=$BUILD_DIR/$install_dir/usr/bin:\$PATH" >"$script"
 # The variable GRAPHVIZ_DOT is needed by some applications (e.g. PlantUML)
-echo "export GRAPHVIZ_DOT=/app/$install_dir/usr/bin/dot" >>"$script"
+echo "export GRAPHVIZ_DOT=$BUILD_DIR/$install_dir/usr/bin/dot" >>"$script"
 # If shared libraries were installed (necessary on Heroku-16), set load path
 ((${#packages[@]} > 1)) &&
-  echo "export LD_LIBRARY_PATH=/app/$install_dir/usr/lib:\$LD_LIBRARY_PATH" >>"$script"
+  echo "export LD_LIBRARY_PATH=$BUILD_DIR/$install_dir/usr/lib:\$LD_LIBRARY_PATH" >>"$script"
 
 echo "Successfully installed Graphviz $graphviz_version"
 echo "Verify installation with \"heroku run dot -V\""
