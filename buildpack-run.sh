65a510fd4b97b22ca4f477f55ef00f9811404086
Jimbo Freedman <github@jlf.me>
2017-08-31 20:17:09 +0100

    Install graphviz on deploy

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,7 @@
 wget http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.40.1.tar.gz
 tar -zxf graphviz-2.40.1.tar.gz
 cd graphviz-2.40.1
-./configure
+./configure --prefix=/app/graphviz
 make
 make install
 
