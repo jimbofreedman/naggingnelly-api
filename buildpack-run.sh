514cb2d9697279a9971b809de3503345de7152cf
Jimbo Freedman <github@jlf.me>
2017-08-31 21:23:19 +0100

    Install graphviz on deploy

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -4,9 +4,9 @@ mkdir -p /app/.heroku/vendor/bin
 BASE_DIR=`pwd`
 wget http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.40.1.tar.gz
 tar -zxf graphviz-2.40.1.tar.gz
-cd graphviz-2.40.1
+pushd graphviz-2.40.1
 ./configure --prefix=/app/graphviz --disable-shared
 make
 make install DESTDIR=$BASE_DIR/tmpdestdir
-cd $BASE_DIR
+popd
 cp -R "tempdestdir" "$BUILD_DIR/graphviz"
