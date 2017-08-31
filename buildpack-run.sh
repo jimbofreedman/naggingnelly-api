dec545879289b0772bfcf2746d5717a4e4258c72
Jimbo Freedman <github@jlf.me>
2017-08-31 21:32:57 +0100

    Install graphviz on deploy

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -5,7 +5,7 @@ BASE_DIR=`pwd`
 wget http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.40.1.tar.gz
 tar -zxf graphviz-2.40.1.tar.gz
 pushd graphviz-2.40.1
-./configure --prefix=/app/graphviz --disable-shared
+./configure --disable-shared
 make
 make install DESTDIR=$BASE_DIR/tmpdestdir
 popd
