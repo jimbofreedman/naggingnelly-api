d6e4b2017858c2cda71eeb182bcffda0e2622b8f
Jimbo Freedman <github@jlf.me>
2018-01-15 13:29:42 +0000

    Fix tests on Travis

--- gtd/tests.py
+++ gtd/tests.py
@@ -97,7 +97,7 @@ class ActionModelTests(TestCase):
         action.status = action.STATUS_COMPLETED
         action.save()
         self.assertIs(action.status, action.STATUS_OPEN)
-        action_recurrence = ActionRecurrence.objects.get(pk=1)
+        action_recurrence = ActionRecurrence.objects.get(action=action)
         self.assertIs(action_recurrence.action.id, action.id)
         self.assertIs(action_recurrence.status, action.STATUS_COMPLETED)
         self.assertEqual(action_recurrence.start_at, start_at)
@@ -142,7 +142,7 @@ class ActionModelTests(TestCase):
         action.status = action.STATUS_COMPLETED
         action.save()
         self.assertIs(action.status, action.STATUS_OPEN)
-        action_recurrence = ActionRecurrence.objects.get(pk=1)
+        action_recurrence = ActionRecurrence.objects.get(action=action)
         self.assertIs(action_recurrence.action.id, action.id)
         self.assertIs(action_recurrence.status, action.STATUS_COMPLETED)
         self.assertEqual(action_recurrence.start_at, start_at)
