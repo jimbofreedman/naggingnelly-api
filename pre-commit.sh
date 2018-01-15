584c07f9cf9c75d2cfbb168da3598e630f1bcd71
Jimbo Freedman <github@jlf.me>
2018-01-15 03:55:33 +0000

    pre-commit.sh

---
+++ pre-commit.sh
@@ -0,0 +1,15 @@
+# pre-commit.sh
+set -e
+
+STASH_NAME="pre-commit-$(date +%s)"
+git stash save -q --keep-index $STASH_NAME
+
+# Test prospective commit
+pycodestyle
+coverage run manage.py test
+coverage report
+
+STASHES=$(git stash list)
+if [[ $STASHES == "$STASH_NAME" ]]; then
+  git stash pop -q
+fi
