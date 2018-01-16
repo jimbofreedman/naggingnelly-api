9364fed50acd0c08cd64e001e43a349c32501d1d
Jimbo Freedman <github@jlf.me>
2018-01-16 15:25:27 +0000

    Add ScentGroup to Admin

--- smelltest/admin.py
+++ smelltest/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Scent, TestResult
+from .models import ScentGroup, Scent, TestResult
 
+admin.site.register(ScentGroup)
 admin.site.register(TestResult)
 
 
