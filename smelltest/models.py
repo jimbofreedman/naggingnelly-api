f35fb850a20b6d1fa46ff2f8496af4d5cda3cd28
Jimbo Freedman <github@jlf.me>
2018-01-16 16:05:14 +0000

    ScentGroup color; reformatting; HTTPS

--- smelltest/models.py
+++ smelltest/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class ScentGroup(models.Model):
     name = models.CharField(max_length=30)
+    color = models.CharField(max_length=6, default='ff0000')
 
     def __str__(self):
         return self.name
@@ -21,3 +22,6 @@ class TestResult(models.Model):
     scent = models.ForeignKey(Scent, related_name='tests')
     guess = models.ForeignKey(Scent, related_name='guesses')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '#{} {} as #{} {}'.format(self.scent.id, self.scent.name, self.guess.id, self.guess.name)
