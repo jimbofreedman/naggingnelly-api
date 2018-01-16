98e339930fd3075a2b3e06862ba81fa409a4f441
Jimbo Freedman <github@jlf.me>
2018-01-16 15:26:40 +0000

    Display ScentGroup name

--- smelltest/models.py
+++ smelltest/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class ScentGroup(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Scent(models.Model):
     name = models.CharField(max_length=30)
@@ -11,7 +14,7 @@ class Scent(models.Model):
     enabled = models.BooleanField(default=False)
 
     def __str__(self):
-        return'#{} {}'.format(self.id, self.name)
+        return '#{} {}'.format(self.id, self.name)
 
 
 class TestResult(models.Model):
