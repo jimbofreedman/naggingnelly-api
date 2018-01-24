6570809a7881924058949bcbf559af77e4b1d616
Jimbo Freedman <github@jlf.me>
2018-01-24 21:55:21 +0000

    TokenAuthentication for API

--- config/settings/base.py
+++ config/settings/base.py
@@ -305,5 +305,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
+
+REST_SESSION_LOGIN = False
