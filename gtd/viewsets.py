66db2e05ff1d7eaf56f5cf054ea3143b3af35457
Jimbo Freedman <github@jlf.me>
2018-01-27 18:18:21 +0000

    Remove Graphviz and related API calls - moving them client-side

--- gtd/viewsets.py
+++ gtd/viewsets.py
@@ -82,23 +82,3 @@ class ActionViewSet(APIViewSet):
         action.save()
         return Response(self.get_serializer(action).data)
 
-    def _get_graph(self, format):
-        dot = Digraph(format=format, comment='Tasks', graph_attr={"rankdir": "LR"})
-        actions = self.get_queryset().filter(status=Action.STATUS_OPEN)
-        for a in actions:
-            dot.node(str(a.id), a.short_description)
-            for d in a.depends_on.all():
-                dot.edge(str(a.id), str(d.id))
-        return dot
-
-    @list_route(methods=['get'])
-    def graph_png(self, request):
-        return HttpResponse(self._get_graph("png").pipe(), content_type="image/png")
-
-    @list_route(methods=['get'])
-    def graph_json(self, request):
-        return HttpResponse(self._get_graph("json").pipe(), content_type="application/json")
-
-    @list_route(methods=['get'])
-    def graph_svg(self, request):
-        return HttpResponse(self._get_graph("svg").pipe(), content_type="text/plain")
