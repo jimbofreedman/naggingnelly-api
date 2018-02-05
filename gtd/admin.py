a944309412d2b5d3cc19374b8efbadd49b7c22b4
Jimbo Freedman <github@jlf.me>
2018-02-05 12:07:08 +0000

    Show ActionRecurrences in ActionAdmin

--- gtd/admin.py
+++ gtd/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
-from .models import Action, Context, Folder, GtdUser
+from .models import Action, ActionRecurrence, Context, Folder, GtdUser
 
 admin.site.register(GtdUser)
 admin.site.register(Folder)
 admin.site.register(Context)
-admin.site.register(Action)
+
+
+class ActionRecurrenceInline(admin.TabularInline):
+    model = ActionRecurrence
+    extra = 0
+
+
+class ActionAdmin(admin.ModelAdmin):
+    inlines = [
+        ActionRecurrenceInline,
+    ]
+
+
+admin.site.register(Action, ActionAdmin)
