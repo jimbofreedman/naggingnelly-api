005ab4eb15202af774dce320736cad25569d5cb0
Jimbo Freedman <github@jlf.me>
2018-04-01 17:49:16 +0100

    Serialize updated_at for client

--- gtd/serializers.py
+++ gtd/serializers.py
@@ -6,23 +6,23 @@ from .models import Action, Context, Folder, GtdUser
 class GtdUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = GtdUser
-        fields = ['bin', 'collectbox', 'actions', 'waiting_for', 'tickler', 'someday']
+        fields = ['bin', 'collectbox', 'actions', 'waiting_for', 'tickler', 'someday', 'updated_at']
 
 
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Folder
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'updated_at']
 
 
 class ContextSerializer(serializers.ModelSerializer):
     class Meta:
         model = Context
-        fields = ['id', 'name', 'glyph']
+        fields = ['id', 'name', 'glyph', 'updated_at']
 
 
 class ActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Action
         fields = ['id', 'short_description', 'status', 'priority', 'start_at', 'due_at', 'recurrence', 'dependencies',
-                  'folder', 'context']
+                  'folder', 'context', 'updated_at']
