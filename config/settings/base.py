f32deb9dbcc4c12cce68e51942819063fd9f7603
Jimbo Freedman <jimbo@project-sapiens.com>
2018-07-18 03:42:08 +0100

    Disable BodyTokenAuthentication (used for orignal Android app)

--- config/settings/base.py
+++ config/settings/base.py
@@ -314,7 +314,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'api.authentication.BodyTokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
