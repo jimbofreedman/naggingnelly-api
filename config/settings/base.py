5b7d5263dda63ce3159e656a9d2f814a8c64a4b3
Jimbo Freedman <jimbo@project-sapiens.com>
2018-07-26 01:25:12 +0100

    Tests for BodyAuthentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -327,7 +327,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_FILTER_BACKENDS': ('gtd.filters.UpdatedSinceFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('gtd.filters.UpdatedSinceFilterBackend',),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 REST_SESSION_LOGIN = False
---
+++ gtd/tests/test_auth.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from faker import Faker
+from django.urls import reverse
+from api.users.models import User
+from rest_framework.test import RequestsClient
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+class ApiAuthTests(TestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.user = User.objects.create(username=self.faker.name())
+        self.token = Token.objects.create(user=self.user)
+        self.body = {"authentication_token": self.token.key,
+                     "shortDescription": "hi",
+                     "folder": self.user.folder_set.first().id,
+                     "context": self.user.context_set.first().id
+                     }
+        self.client = RequestsClient()
+        self.client.headers.update({'x-test': 'true'})
+
+    def get_url(self, name, args=None):
+        return 'https://testserver' + reverse(name, args=args)
+
+    def test_token_header(self):
+        self.client.headers.update({'Authorization': 'Token {}'.format(self.token.key)})
+        response = self.client.post(self.get_url('gtd:actions-list'), json=self.body)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_token_body(self):
+        response = self.client.post(self.get_url('gtd:actions-list'), json=self.body)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
